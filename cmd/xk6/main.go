b96e032e41d26ed328d79efb6bc8fe5773965a21
Nikita Aleksandrov <n.aleksandrov@arenadata.io>
2025-10-01 23:34:35 +0300

    feat(k6,config): k6 opts to unit_queue, -1 iterations
    Closes RNDSTROPPY-17

--- cmd/xk6/main.go
+++ cmd/xk6/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/stroppy-io/stroppy/pkg/common/logger"
+	"github.com/stroppy-io/stroppy/pkg/common/proto"
 	stroppy "github.com/stroppy-io/stroppy/pkg/common/proto"
 	"github.com/stroppy-io/stroppy/pkg/common/unit_queue"
 	"github.com/stroppy-io/stroppy/pkg/driver"
@@ -28,28 +29,49 @@ func init() { //nolint:gochecknoinits // allow for xk6
 
 	runContextBytes, ok := os.LookupEnv("context")
 	if !ok {
-		lg.Fatal("no context provided, fatal error")
+		lg.Panic("no context provided, fatal error")
 	}
 
-	runContext, err := Serialized[*stroppy.StepContext](runContextBytes).Unmarshal()
+	stepContext, err := Serialized[*stroppy.StepContext](runContextBytes).Unmarshal()
 	if err != nil {
-		lg.Fatal("can't deserialize step context", zap.Error(err))
+		lg.Panic("can't deserialize step context", zap.Error(err))
 	}
 
-	lg.Debug("xk6 module init", zap.Uint64("seed", runContext.GetConfig().GetSeed()))
+	lg.Debug("xk6 module init", zap.Uint64("seed", stepContext.GetConfig().GetSeed()))
 
 	processCtx := context.Background()
 
-	drv := driver.Dispatch(lg, runContext.GetConfig().GetDriver())
+	drv := driver.Dispatch(lg, stepContext.GetConfig().GetDriver())
 
-	err = drv.Initialize(processCtx, runContext)
+	err = drv.Initialize(processCtx, stepContext)
 	if err != nil {
-		lg.Fatal("driver isn't initialized", zap.Error(err))
+		lg.Panic("driver isn't initialized", zap.Error(err))
 	}
 
-	// TODO: solve limits and buffers with k6 scenario config from runContext.GetExecutor().GetK6().GetScenario().GetExecutor()
-	queue := unit_queue.NewQueue(drv.GenerateNextUnit, 0, 1)
-	for _, u := range runContext.GetWorkload().GetUnits() {
+	var workersLimit int
+	switch exec := stepContext.GetExecutor().GetK6().GetScenario().GetExecutor().(type) {
+	case *proto.K6Scenario_ConstantArrivalRate:
+		workersLimit = int(exec.ConstantArrivalRate.GetMaxVus())
+	case *proto.K6Scenario_ConstantVus:
+		workersLimit = int(exec.ConstantVus.GetVus())
+	case *proto.K6Scenario_PerVuIterations:
+		workersLimit = int(exec.PerVuIterations.GetVus())
+	case *proto.K6Scenario_RampingArrivalRate:
+		workersLimit = int(exec.RampingArrivalRate.GetMaxVus())
+	case *proto.K6Scenario_RampingVus:
+		workersLimit = int(exec.RampingVus.GetMaxVus())
+	case *proto.K6Scenario_SharedIterations:
+		workersLimit = int(exec.SharedIterations.GetVus())
+	default:
+		lg.Panic("unexpected proto.isK6Scenario_Executor")
+	}
+	if !stepContext.GetWorkload().GetAsync() {
+		workersLimit = 1
+	}
+
+	// TODO: figure out how to tune the bufferSize better
+	queue := unit_queue.NewQueue(drv.GenerateNextUnit, workersLimit, workersLimit)
+	for _, u := range stepContext.GetWorkload().GetUnits() {
 		queue.PrepareGenerator(u.GetDescriptor_(), 1, uint(u.GetCount()))
 	}
 
@@ -58,7 +80,7 @@ func init() { //nolint:gochecknoinits // allow for xk6
 	runPtr = newRuntimeContext(
 		drv,
 		lg,
-		runContext,
+		stepContext,
 		queue,
 	)
 
